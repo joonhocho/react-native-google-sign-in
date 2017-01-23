db805b2d014ad7dd43ffbebfe1e1816c19bf5ba3
Joon Ho Cho <joonho1101@gmail.com>
2017-01-23 07:22:39 -0800

    swift bridge header

---
+++ ios/RNGoogleSignIn/RNGoogleSignIn-Bridging-Header.h
@@ -0,0 +1,9 @@
+#ifndef RNGoogleSignIn_Bridging_Header_h
+#define RNGoogleSignIn_Bridging_Header_h
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTViewManager.h>
+#import <React/RCTEventEmitter.h>
+#import <Google/SignIn.h>
+
+#endif /* RNGoogleSignIn_Bridging_Header_h */

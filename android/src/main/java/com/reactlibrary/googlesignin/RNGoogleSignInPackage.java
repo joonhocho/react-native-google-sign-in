879f4675be3c9e1022035de3821e040422c5fbcd
Ivan Pusic <pusic007@gmail.com>
2017-08-09 21:18:33 +0200

    fix react-native 0.47 compatibility
    this is an urgent fix to support latest react-native version.

--- android/src/main/java/com/reactlibrary/googlesignin/RNGoogleSignInPackage.java
+++ android/src/main/java/com/reactlibrary/googlesignin/RNGoogleSignInPackage.java
@@ -16,7 +16,7 @@ public class RNGoogleSignInPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNGoogleSignInModule(reactContext));
     }
 
-    @Override
+    // Depreciated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
